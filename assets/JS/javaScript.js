0a9414fbfe9d8cfd91ef8e97790c667c3d0191a7
kauancamilosenai <kauan_camilo@estudante.sesisenai.org.br>
2026-03-10 16:55:07 -0300

    js atualizado sem virus 2027
    addddddddddddddddddddd

--- assets/JS/javaScript.js
+++ assets/JS/javaScript.js
@@ -1,1 +1,41 @@
-// quando clicar em tentativa: aparecer o esqueleto 
+const palavrasAleatorias = ["Chaves" , "Barril" , "Carros" , "Itens" ,
+ "Spoiler" , "Seis" , "Transporte" , "Publicidade" , "Onze" , "SantaCatarina"];
+
+
+let palavraSecreta = "";   //guarda a palavra que foi sorteada
+let tentativasRestantes = 6;   
+let letrasUsadas = [];
+let letrasCorretas = [];
+let displayPalavra = [];
+let pontuacao = 0;
+
+const displayPalavra = document.getElementById(palavraSecreta);
+const displayTentativas = document.getElementById(tentativasRestantes);
+const displayPontuacao = Document.getElementById(pontuacao);
+const botaoReinicio = document.getElementById(restartButton);
+
+function iniciarJogo(){
+    const posicaoSorteadaDaListaDePalavras = Math.floor(math.random() * palavrasAleatorias.length);
+    
+    palavraSecreta = palavrasAleatorias[posicaoSorteadaDaListaDePalavras].fill(_);
+
+    //reset limpando todos os dados
+    tentativasRestantes = 6;
+    letrasUsadas = [];
+    letrasCorretas = [];
+   
+    renderizarPalavra();
+}
+
+function renderizarPalavra(){
+    palavraSecreta.innerHTML = ""
+    displayPalavra.forEach(letra => {
+        const span = document.createElement("span");
+        span.innerText = letra;
+        palavraSecreta.appendChild(span);
+    }
+    )
+}
+
+botaoReinicio.addEventListener("click", iniciarJogo);
+iniciarJogo();
